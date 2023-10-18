a087c8af4dc3d462f3220aa4dd301858072217e8
srs-sieam <srssieam@gmail.com>
2023-10-18 06:40:20 +0600

    Basic environment setup done. here I have installed react router dom, tailwind css, react icon, daisy ui, google font

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<div>Hellow world</div>
+    }
+])
+export default Router;
