7ff94f8f6ef4da4c911b59e7b1cdf5649cbcb6af
srs-sieam <srssieam@gmail.com>
2023-10-19 23:21:46 +0600

    added footer

--- src/root/MainLayout.jsx
+++ src/root/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Header/Navbar";
+import Footer from "../components/footer/Footer";
 
 
 const MainLayout = () => {
@@ -7,6 +8,7 @@ const MainLayout = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
